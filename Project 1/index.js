440cd0d28076ffe251adb47d3092a12c8618ea20
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-04-10 01:34:11 +0530

    CRUD operations

--- Project 1/index.js
+++ Project 1/index.js
@@ -1,21 +1,28 @@
-const express = require('express');
-const users = require("./MOCK_DATA.json")
+const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
 
 // ROUTES
-app.get("/api/users", (req, res) => {
-    res.json(users);
-})
-
 app.get("/users", (req, res) => {
-    const html = `
-    <ul>
-        ${users.map(user => `<li>${user.first_name}</li>`).join("")}
-    </ul>
-    `
-    return res.send(html)
+  const html = `
+      <ul>
+          ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+      </ul>
+      `;
+  return res.send(html);
+});
+
+// REST API
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id)
+    return res.json(user)
 })
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
