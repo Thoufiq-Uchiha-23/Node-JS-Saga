c4d844e5f93d3173d5ce6f4a43d8153198a76bbe
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-04-11 21:35:10 +0530

    adding other methods

--- Project 1/index.js
+++ Project 1/index.js
@@ -19,10 +19,41 @@ app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id)
-    return res.json(user)
-})
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .put((req, res) => {
+    // Edit the user with id
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    // Delete the user with id
+    return res.json({ status: "pending" });
+  });
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+app.post("/api/users", (req, res) => {
+  // TODO: Create new user
+  return res.json({ status: "pending" });
+});
+
+app.patch("/api/users/:id", (req, res) => {
+  // TODO: Edit the user with id
+  return res.json({ status: "pending" });
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  // TODO: Delete the user with id
+  return res.json({ status: "pending" });
+});
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
