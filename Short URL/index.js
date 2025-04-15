5e0b97653cb9fa84e57337efc87dad7be405cbc1
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-04-16 01:44:07 +0530

    index file

---
+++ Short URL/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url.js");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
+  console.log("Mongodb connected");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/url", urlRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
