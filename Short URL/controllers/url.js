0b725c693851d67765b9a22773a93be54ee2ec32
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-04-17 21:29:21 +0530

    minor change

--- Short URL/controllers/url.js
+++ Short URL/controllers/url.js
@@ -5,14 +5,14 @@ const generateShortURL = async (req, res) => {
   const body = req.body;
   if (!body.url) return res.status(400).json({ error: "url is required" });
   const shortID = nanoid(8);
-  console.log(body)
+  console.log(body);
   await URL.create({
     shortId: shortID,
     redirectURL: body.url,
     visitHistory: [],
   });
 
-  console.log("short URL",body.url)
+  console.log("short URL", body.url);
   return res.json({ id: shortID });
 };
 
@@ -22,9 +22,9 @@ const getAnalytics = async (req, res) => {
   return res.json({
     totalClicks: result.visitHistory.length,
     analytics: result.visitHistory,
-  })
-}
+  });
+};
 module.exports = {
   generateShortURL,
   getAnalytics,
-}
+};
