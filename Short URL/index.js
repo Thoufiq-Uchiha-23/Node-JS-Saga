35c269f106a301b1988819741ec45d12734aeb3f
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-04-18 01:42:28 +0530

    ejs embed

--- Short URL/index.js
+++ Short URL/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
 
 const { connectToMongoDB } = require("./connect");
 const urlRoute = require("./routes/url.js");
+const staticRoute = require("./routes/staticRouter.js")
 const URL = require("./models/url.js");
 const app = express();
 const PORT = 8001;
@@ -10,11 +12,17 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
   console.log("Mongodb connected");
 });
 
+// Setting ejs
+app.set("view engine", "ejs");
+// To tell the path where all views are present
+app.set("views", path.resolve("./views") )
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/url", urlRoute);
+app.use("/", staticRoute)
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
