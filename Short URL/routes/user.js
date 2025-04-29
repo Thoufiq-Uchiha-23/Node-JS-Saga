b5731ff706127f51d7f42de2c0e7d646748539bf
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-04-29 16:04:42 +0530

    user routes

---
+++ Short URL/routes/user.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const { userSignUp } = require("../controllers/user");
+const router = express.Router();
+
+router.post("/", userSignUp);
+module.exports = router;
