a001845c5efc962288ef877aa1e02d66c685f3d2
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-04-29 16:04:52 +0530

    user middleware

--- Short URL/index.js
+++ Short URL/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const { connectToMongoDB } = require("./connect");
-const urlRoute = require("./routes/url.js");
-const staticRoute = require("./routes/staticRouter.js")
+
 const URL = require("./models/url.js");
 const app = express();
 const PORT = 8001;
+
+const urlRoute = require("./routes/url.js");
+const staticRoute = require("./routes/staticRouter.js")
+const userRoute = require("./routes/user.js")
 
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
   console.log("Mongodb connected");
@@ -21,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/url", urlRoute);
 app.use("/", staticRoute)
+app.use("/user", userRoute)
 
 app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
