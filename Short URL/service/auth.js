478a98758d2aa8ea47127be6f2a9e9c0202c5f4d
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-05-01 22:06:16 +0530

    service auth json

--- Short URL/service/auth.js
+++ Short URL/service/auth.js
@@ -1,14 +1,44 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+// function setUser(id, user) {
+//     sessionIdToUserMap.set(id, user);
+// }
+
+// function getUser(id) {
+//     return sessionIdToUserMap.get(id);
+// }
+
+// module.exports = {
+//     setUser,
+//     getUser,
+// }
+
+// JWT Token Method
+const jwt = require("jsonwebtoken");
+const secret = "Thoufiq$123@$";
+
+function setUser(user) {
+  return jwt.sign(
+    {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    secret,
+  );
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) {
+    console.log("No token provided");
+    return null;
+  }
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    console.error("JWT Error:", error.message);
+    return null;
+  }
 }
 
-module.exports = {
-    setUser,
-    getUser,
-}
+module.exports = { setUser, getUser };
