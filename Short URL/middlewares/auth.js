6cf05e69c6093aaa63a1cd1d60a543a53126fd9d
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-05-07 01:43:32 +0530

    middlewares

--- Short URL/middlewares/auth.js
+++ Short URL/middlewares/auth.js
@@ -1,21 +1,37 @@
 const { getUser } = require("../service/auth.js");
 
 function checkForAuthentication(req, res, next) {
-  const authorizationHeaderValue = req.headers["authorization"];
-  req.user = null;
-  if (
-    !authorizationHeaderValue ||
-    !authorizationHeaderValue.startsWith("Bearer ")
-  )
-    return next();
+  // AUTH WAY
+  //   const authorizationHeaderValue = req.cookies?.token;
+  //   req.user = null;
+  //   if (
+  //     !authorizationHeaderValue ||
+  //     !authorizationHeaderValue.startsWith("Bearer ")
+  //   )
+  //     return next();
+  //  const token = authorizationHeaderValue.split("Bearer ")[1]; //Bearer 13519yy9gao95y0qy0
 
-    const token = authorizationHeaderValue.split("Bearer ")[1]; //Bearer 13519yy9gao95y0qy0
-    const user = getUser(token)
+  //   TOKEN WAY
+  const tokenCookie = req.cookies?.token;
+  const token = tokenCookie;
+  
+  const user = getUser(token);
 
-    req.user = user;
-    return next();
+  req.user = user;
+  return next();
+}
+
+function restrictTo(roles) {
+  return function (req, res, next) {
+    if (!req.user) return res.redirect("/login");
+
+    if (!roles.includes(req.user.role)) return res.end("UnAuthorized");
+  };
+
+  return next();
 }
 
 module.exports = {
   checkForAuthentication,
+  restrictTo,
 };
